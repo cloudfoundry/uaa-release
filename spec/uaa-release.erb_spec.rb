e987f683ba02c297fbc0227291536efb9d72616c
Filip Hanik <fhanik@pivotal.io>
2016-12-08 14:02:53 -0800

    Restructure code for reuse Add tests to validate required properties. Still need to validate the message UAA key validation - basic

--- spec/uaa-release.erb_spec.rb
+++ spec/uaa-release.erb_spec.rb
@@ -58,27 +58,50 @@ describe 'uaa-release erb generation' do
   end
 
 
+  def self.read_and_parse_string_template(template, manifest, asYaml)
+    erbTemplate = File.read(File.join(File.dirname(__FILE__), template))
+    if asYaml
+      completedTemplate = perform_erb_transformation_as_yaml(erbTemplate, manifest)
+    else
+      completedTemplate = perform_erb_transformation_as_string(erbTemplate, manifest)
+    end
+    completedTemplate
+  end
+
   def self.perform_compare input, output_uaa, output_login, output_log4j
-    uaa_erb_yaml = File.read(File.join(File.dirname(__FILE__), '../jobs/uaa/templates/uaa.yml.erb'))
-    login_erb_yaml =  File.read(File.join(File.dirname(__FILE__), '../jobs/uaa/templates/login.yml.erb'))
-    log4j_erb_properties =  File.read(File.join(File.dirname(__FILE__), '../jobs/uaa/templates/log4j.properties.erb'))
-
     generated_cf_manifest = generate_cf_manifest(input)
-    parsed_uaa_yaml = perform_erb_transformation_as_yaml(uaa_erb_yaml, generated_cf_manifest)
-    parsed_login_yaml = perform_erb_transformation_as_yaml(login_erb_yaml, generated_cf_manifest)
-    parsed_log4j_properties = perform_erb_transformation_as_string(log4j_erb_properties, generated_cf_manifest)
 
-    it "uaa.yml should match" do
+    parsed_uaa_yaml = read_and_parse_string_template '../jobs/uaa/templates/uaa.yml.erb', generated_cf_manifest, true
+    parsed_login_yaml =  read_and_parse_string_template '../jobs/uaa/templates/login.yml.erb', generated_cf_manifest, true
+    parsed_log4j_properties =  read_and_parse_string_template '../jobs/uaa/templates/log4j.properties.erb', generated_cf_manifest, false
+
+    doSaveOutput = ENV['TEST_SAVE'] ? ENV['TEST_SAVE'] : "false"
+
+    if doSaveOutput == "true"
+      tempDir = '/tmp/uaa-release-tests/'+input
+      FileUtils.mkdir_p tempDir
+      open(tempDir + '/uaa.yml', 'w') { |f|
+        f.puts parsed_uaa_yaml.to_yaml(:Indent => 2, :UseHeader => true, :UseVersion => true)
+      }
+      open(tempDir + '/login.yml', 'w') { |f|
+        f.puts parsed_login_yaml.to_yaml(:Indent => 2, :UseHeader => true, :UseVersion => true)
+      }
+      open(tempDir + '/log4j.properties', 'w') { |f|
+        f.puts parsed_log4j_properties.to_s
+      }
+    end
+
+    it "uaa.yml for "+input+" must match" do
       expected = File.read(output_uaa)
       actual = parsed_uaa_yaml.to_yaml
       expect(actual).to yaml_eq(expected)
     end
-    it "login.yml should match" do
+    it "login.yml for "+input+" must match" do
       expected = File.read(output_login)
       actual = parsed_login_yaml.to_yaml
       expect(actual).to yaml_eq(expected)
     end
-    it "log4j.properties should match" do
+    it "log4j.properties for "+input+" must match" do
       expected = File.read(output_log4j)
       actual = parsed_log4j_properties.to_s
       expect(actual).to eq(expected)
@@ -86,8 +109,55 @@ describe 'uaa-release erb generation' do
 
   end
 
+  def self.validate_required_properties input
+    generated_cf_manifest = generate_cf_manifest(input)
+
+    login_yaml_required_properties = [
+      'properties.login.saml.serviceProviderKey',
+      'properties.login.saml.serviceProviderKeyPassword',
+      'properties.login.saml.serviceProviderCertificate'
+    ]
+
+    for addProperty in login_yaml_required_properties
+      begin
+        read_and_parse_string_template '../jobs/uaa/templates/login.yml.erb', generated_cf_manifest, true
+        raise 'This line should not be reached'
+      rescue ArgumentError
+          # expected
+      end
+      add_param_to_hash addProperty, 'value', generated_cf_manifest
+    end
+    read_and_parse_string_template '../jobs/uaa/templates/login.yml.erb', generated_cf_manifest, true
+
+    begin
+      read_and_parse_string_template '../jobs/uaa/templates/uaa.yml.erb', generated_cf_manifest, true
+      raise 'This line should not be reached'
+    rescue ArgumentError
+      # expected
+    end
+    add_param_to_hash 'properties.uaa.jwt.signing_key', 'value', generated_cf_manifest
+    read_and_parse_string_template '../jobs/uaa/templates/uaa.yml.erb', generated_cf_manifest, true
+    generated_cf_manifest['properties']['uaa']['jwt']['signing_key'] = nil
+    begin
+      read_and_parse_string_template '../jobs/uaa/templates/uaa.yml.erb', generated_cf_manifest, true
+      raise 'This line should not be reached'
+    rescue ArgumentError
+      # expected
+    end
+    add_param_to_hash 'properties.uaa.jwt.policy.active_key_id', 'value', generated_cf_manifest
+    read_and_parse_string_template '../jobs/uaa/templates/uaa.yml.erb', generated_cf_manifest, true
+    generated_cf_manifest['properties']['uaa']['jwt']['policy']['active_key_id'] = nil
+    begin
+      read_and_parse_string_template '../jobs/uaa/templates/uaa.yml.erb', generated_cf_manifest, true
+      raise 'This line should not be reached'
+    rescue ArgumentError
+      # expected
+    end
+
+  end
+
+  validate_required_properties 'spec/input/missing-required-properties.yml'
   perform_compare 'spec/input/bosh-lite.yml', 'spec/compare/bosh-lite-uaa.yml', 'spec/compare/bosh-lite-login.yml', 'spec/compare/default-log4j.properties'
   perform_compare 'spec/input/all-properties-set.yml', 'spec/compare/all-properties-set-uaa.yml', 'spec/compare/all-properties-set-login.yml', 'spec/compare/all-properties-set-log4j.properties'
   perform_compare 'spec/input/test-defaults.yml', 'spec/compare/test-defaults-uaa.yml', 'spec/compare/test-defaults-login.yml', 'spec/compare/default-log4j.properties'
-
 end

783c5bc1af2b11dea78b42edd8bf17b18e6c18ab
Filip Hanik <fhanik@pivotal.io>
2016-12-08 08:51:47 -0800

    Add in testing for log4j.properties generated file Important given that IntelliJ likes to trim traililng spaces (like "vcap:uaa ")

--- spec/uaa-release.erb_spec.rb
+++ spec/uaa-release.erb_spec.rb
@@ -48,20 +48,25 @@ describe 'uaa-release erb generation' do
     manifest_hash
   end
 
-  def self.perform_erb_transformation erb_file, manifest_file
+  def self.perform_erb_transformation_as_yaml erb_file, manifest_file
+    YAML.load(perform_erb_transformation_as_string erb_file, manifest_file)
+  end
+
+  def self.perform_erb_transformation_as_string erb_file, manifest_file
     binding = Bosh::Template::EvaluationContext.new(manifest_file).get_binding
-    YAML.load(ERB.new(erb_file).result(binding))
+    ERB.new(erb_file).result(binding)
   end
 
 
-
-  def self.perform_compare input, output_uaa, output_login
+  def self.perform_compare input, output_uaa, output_login, output_log4j
     uaa_erb_yaml = File.read(File.join(File.dirname(__FILE__), '../jobs/uaa/templates/uaa.yml.erb'))
     login_erb_yaml =  File.read(File.join(File.dirname(__FILE__), '../jobs/uaa/templates/login.yml.erb'))
+    log4j_erb_properties =  File.read(File.join(File.dirname(__FILE__), '../jobs/uaa/templates/log4j.properties.erb'))
 
     generated_cf_manifest = generate_cf_manifest(input)
-    parsed_uaa_yaml = perform_erb_transformation(uaa_erb_yaml, generated_cf_manifest)
-    parsed_login_yaml = perform_erb_transformation(login_erb_yaml, generated_cf_manifest)
+    parsed_uaa_yaml = perform_erb_transformation_as_yaml(uaa_erb_yaml, generated_cf_manifest)
+    parsed_login_yaml = perform_erb_transformation_as_yaml(login_erb_yaml, generated_cf_manifest)
+    parsed_log4j_properties = perform_erb_transformation_as_string(log4j_erb_properties, generated_cf_manifest)
 
     it "uaa.yml should match" do
       expected = File.read(output_uaa)
@@ -73,11 +78,16 @@ describe 'uaa-release erb generation' do
       actual = parsed_login_yaml.to_yaml
       expect(actual).to yaml_eq(expected)
     end
+    it "log4j.properties should match" do
+      expected = File.read(output_log4j)
+      actual = parsed_log4j_properties.to_s
+      expect(actual).to eq(expected)
+    end
 
   end
 
-  perform_compare 'spec/input/bosh-lite.yml', 'spec/compare/bosh-lite-uaa.yml', 'spec/compare/bosh-lite-login.yml'
-  perform_compare 'spec/input/all-properties-set.yml', 'spec/compare/all-properties-set-uaa.yml', 'spec/compare/all-properties-set-login.yml'
-  perform_compare 'spec/input/test-defaults.yml', 'spec/compare/test-defaults-uaa.yml', 'spec/compare/test-defaults-login.yml'
+  perform_compare 'spec/input/bosh-lite.yml', 'spec/compare/bosh-lite-uaa.yml', 'spec/compare/bosh-lite-login.yml', 'spec/compare/default-log4j.properties'
+  perform_compare 'spec/input/all-properties-set.yml', 'spec/compare/all-properties-set-uaa.yml', 'spec/compare/all-properties-set-login.yml', 'spec/compare/all-properties-set-log4j.properties'
+  perform_compare 'spec/input/test-defaults.yml', 'spec/compare/test-defaults-uaa.yml', 'spec/compare/test-defaults-login.yml', 'spec/compare/default-log4j.properties'
 
 end

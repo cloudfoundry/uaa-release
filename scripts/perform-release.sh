2f2e3d81f161c0f0f585cec3a7737d00f9eebda4
Filip Hanik <fhanik@pivotal.io>
2017-03-27 16:52:31 -0700

    Update release script so that a release tag contains both metadata and source code for that tag.
    This will also preserve master branch as the source of truth for bosh.io release information.
    
    [#142535405] https://www.pivotaltracker.com/story/show/142535405

--- scripts/perform-release.sh
+++ scripts/perform-release.sh
@@ -7,6 +7,11 @@ CYAN='\033[0;36m'
 BOLD='\033[0;1m'
 NC='\033[0m' # No Color
 
+TMPDIR=/tmp
+SAVEDIR=$TMPDIR/uaa-release-save
+RELEASES=$SAVEDIR/releases
+FINAL_BUILDS=$SAVEDIR/.final_builds
+
 # update submodules
 function sub_update {
     git submodule sync
@@ -43,6 +48,33 @@ function finalize_and_commit {
     git commit --no-verify -m "uaa-release version v${1}"
 }
 
+function copy_master_release_metadata {
+    # make a copy of releases files, we will use these
+    rm -rf $SAVEDIR
+    mkdir -p $SAVEDIR
+    git checkout master
+    cp -r releases $SAVEDIR/
+    cp -r .final_builds $SAVEDIR/
+}
+
+function paste_master_release_metadata {
+    # update metadata in preparation of release
+    echo -e "Merging release metadata"
+    cp -r $RELEASES/* releases/
+    cp -r $FINAL_BUILDS/* .final_builds/
+    git add releases .final_builds
+    if git commit -m "Update bosh release metadata information for version ${1}"; then
+        git push origin $1
+        echo -e "Release metadata merged"
+    else
+       echo -e "No merge required. No changes detected"
+    fi
+}
+
+# =============================================================================
+#                               SCRIPT START
+# =============================================================================
+
 # check our command line arguments
 if [ "$#" -lt 1 ]; then
     usage
@@ -65,6 +97,7 @@ fi
 
 # navigate to the correct release directory
 cd `dirname $0`/..
+echo -e "${NC}Performing release from directory:${GREEN} `pwd` ${NC}"
 
 # initialize sub modules if needed
 sub_update
@@ -80,7 +113,10 @@ fi
 echo "Updating all branches"
 git fetch --all --prune > /dev/null
 
-echo "Creating bosh UAA-release $1 using `bosh -v`"
+# save the metadata files from master
+copy_master_release_metadata
+
+echo "Creating bosh UAA-release ${GREEN} ${1} ${NC} using `bosh -v`"
 
 # just in case it gets deleted during branch switch
 if [ "$#" -ge 3 ]; then
@@ -93,35 +129,47 @@ else
     exit 1
 fi
 
+# jump to the branch that contains our code and sync submodule
 git checkout $branch_to_release_from
-
-# initalize submodule uaa
 sub_update
+
+# paste in our metadata from master and commit it
+paste_master_release_metadata $branch_to_release_from
 
 # restore private.yml in case it got deleted
 mv /tmp/private.yml config/
 
+echo -e "Building tarball ${GREEN}${1}${NC} and tag with ${GREEN}v${1}${NC}"
 # create a release tar ball
 bosh create release --name uaa --version $1 --with-tarball
+metadata_commit=''
+# finalize release, get commit SHA so that we can cherry pick it later
+finalize_and_commit $1 metadata_commit
 
-# we will finalize the release on master branch
+# tag the release
+git tag -a v${1} -m "$1 release"
+git push origin $branch_to_release_from --tags
+
+# jump to master branch
 git checkout master
 git reset --hard origin/master
 sub_update
 
+
 if [ "$branch_to_release_from" = "develop" ]; then
-    # if we are releasing from develop
-    # create a merge commit with the changes
-    git merge --no-ff develop -m "Merge of branch develop for release $1"
-    sub_update
+    # merge metadata files -
+    echo -e "Cherry picking metadata commit to master"
+    git merge --no-ff origin/develop -m "Merge of branch develop for release ${1}"
+else
+    # cherry pick the metadata files - we can't merge. different code paths
+    echo -e "Cherry picking metadata commit to master for release ${1}"
+    git cherry-pick $metadata_commit
 fi
 
-finalize_and_commit $1
-git push origin master
+# update develop (merge master to develop so that the next release won't have a conflict
+git push origin master --tags
 git checkout develop
-git reset --hard origin/develop
-git merge master -m "Merge of branch master for release $1"
-sub_update
+git merge --no-ff master -m "Bumping develop with master contents in preparation of next release"
 git push origin develop
 
 # tag the release

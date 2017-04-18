a8fa0941dc490cf2c4a7eb2e3cdd666fcd3aad04
Jennifer Hamon <jhamon@pivotal.io>
2017-04-18 11:49:04 -0700

    Add some proxy ip configuration specs around tomcat.server.xml
    [#143602943] https://www.pivotaltracker.com/story/show/143602943
    
    Signed-off-by: Helen Chung <hchung@pivotal.io>

--- spec/tomcat.server.xml.erb_spec.rb
+++ spec/tomcat.server.xml.erb_spec.rb
@@ -13,43 +13,97 @@ describe 'tomcat.server.xml' do
   context 'using bosh links' do
     let(:compiled_xml) { compile_erb(template, manifest) }
     let(:template) { '../jobs/uaa/templates/tomcat.server.xml.erb' }
+    let(:internal_proxies) do
+      config = Nokogiri::XML.parse(compiled_xml)
+      config.xpath('//Valve')[0].attributes['internalProxies'].value
+    end
 
-    context 'when uaa.proxy.servers is configured' do
+    context 'when uaa.proxy.servers is configured in the manifest' do
+      before(:each) do
+        manifest['properties']['uaa']['proxy']['servers'] = ["127.1.0.1", "127.1.0.2", "127.1.0.3"]
+        manifest['properties']['uaa']['proxy_ips_regex'] = 'proxy_ips_regex'
+      end
       let(:manifest) { generate_cf_manifest('spec/input/all-properties-set.yml', links) }
       let(:links) {{
           'router' => {'instances' => [{address: 'someaddress'}]}
       }}
 
-      it 'takes precedence over bosh-linked configuration' do
-        config = Nokogiri::XML.parse(compiled_xml)
-        internal_proxies = config.xpath('//Valve')[0].attributes['internalProxies'].value
+      it 'manifest values take precedence over bosh-linked configuration' do
         expect(internal_proxies).not_to include('someaddress')
+        expect(internal_proxies).to include('proxy_ips_regex')
+        expect(internal_proxies).to eq '127\.1\.0\.1|127\.1\.0\.2|127\.1\.0\.3|proxy_ips_regex'
+      end
+    end
+    
+    context 'when uaa.proxy_ips_regex is in the manifest' do
+      let(:manifest) { generate_cf_manifest('spec/input/all-properties-set.yml') }
+
+      it 'includes the proxy_ips_regex when uaa.proxy.servers not set and bosh links not available' do
+        manifest['properties']['uaa']['proxy']['servers'] = []
+        manifest['properties']['uaa']['proxy_ips_regex'] = 'proxy_ips_regex'
+        manifest['links'] = {}
+
+        expect(internal_proxies).to include('proxy_ips_regex')
+      end
+
+      it 'includes proxy_ips_regex when uaa.proxy.servers are set and bosh links are not available' do
+        manifest['properties']['uaa']['proxy']['servers'] = ['1.1.1.1']
+        manifest['properties']['uaa']['proxy_ips_regex'] = 'proxy_ips_regex'
+        manifest['links'] = {}
+
+        expect(internal_proxies).to include('proxy_ips_regex')
+      end
+
+      it 'includes proxy_ips_regex when uaa.proxy.servers not set and bosh link is available' do
+        manifest['properties']['uaa']['proxy']['servers'] = []
+        manifest['properties']['uaa']['proxy_ips_regex'] = 'proxy_ips_regex'
+        manifest['links'] = {
+            'router' => {'instances' => [{'address' => 'linked-address'}]}
+        }
+
+        expect(internal_proxies).to include('proxy_ips_regex')
+      end
+
+      it 'includes proxy_ips_regex when uaa.proxy.servers is set and bosh link is available' do
+        manifest['properties']['uaa']['proxy']['servers'] = ['1.12.3.4']
+        manifest['properties']['uaa']['proxy_ips_regex'] = 'proxy_ips_regex'
+        manifest['links'] = {
+            'router' => {'instances' => [{'address' => 'linked-address'}]}
+        }
+
+        expect(internal_proxies).to include('proxy_ips_regex')
       end
     end
 
-    context 'when uaa.proxy.servers is not congfiured' do
-      before(:each) { manifest['properties']['uaa']['proxy'].delete 'servers' }
-      let(:links) {{
-          'router' => {'instances' => [{'address' => 'linked-address'}]}
-      }}
-      let(:manifest) { generate_cf_manifest('spec/input/all-properties-set.yml', links) }
-
-      it 'tries to fall back to a bosh-linked router config' do
-        config = Nokogiri::XML.parse(compiled_xml)
-        internal_proxies = config.xpath('//Valve')[0].attributes['internalProxies'].value
-        expect(internal_proxies).to include('linked-address')
+    context 'when uaa.proxy.servers is not congfiured in the manifest' do
+      before(:each) do
+        manifest['properties']['uaa']['proxy'].delete 'servers'
+        manifest['properties']['uaa']['proxy_ips_regex'] = 'proxy_ips_regex'
       end
-    end
 
-    context 'when neither uaa.proxy.servers nor bosh-linked router is available' do
-      before(:each) { manifest['properties']['uaa']['proxy'].delete('servers') }
-      let(:links) {{}}
       let(:manifest) { generate_cf_manifest('spec/input/all-properties-set.yml', links) }
 
-      it 'uses a default proxy_ips_regex' do
-        config = Nokogiri::XML.parse(compiled_xml)
-        internal_proxies = config.xpath('//Valve')[0].attributes['internalProxies'].value
-        expect(internal_proxies).not_to be_nil
+      context 'when a bosh-link is available' do
+        let(:links) {{
+            'router' => {'instances' => [{'address' => 'linked-address'}]}
+        }}
+
+        it 'uses the bosh-linked router config' do
+          expect(internal_proxies).to include('proxy_ips_regex')
+          expect(internal_proxies).to include('linked-address')
+        end
+      end
+
+      context 'when there is no bosh-link available' do
+        before(:each) do
+          manifest['properties']['uaa']['proxy_ips_regex'] = ''
+          manifest['properties']['uaa']['proxy']['servers'] = []
+        end
+        let(:links) {{}}
+
+        it 'uses the default internal proxies list' do
+          expect(internal_proxies).to eq '10.d{1,3}.d{1,3}.d{1,3}|192.168.d{1,3}.d{1,3}|169.254.d{1,3}.d{1,3}|127.d{1,3}.d{1,3}.d{1,3}|172.1[6-9]{1}.d{1,3}.d{1,3}|172.2[0-9]{1}.d{1,3}.d{1,3}|172.3[0-1]{1}.d{1,3}.d{1,3}'
+        end
       end
     end
   end

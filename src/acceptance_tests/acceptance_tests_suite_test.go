cb50417bf120d5bfb666a487bb73f73be218e766
Dennis Leon <dleon@pivotal.io>
2018-04-04 11:41:51 -0700

    Add test to ensure every ca cert is loaded into the jdk truststore
    - Added validation and ability to use director lib from bosh-cli
    
    [#156412250] https://www.pivotaltracker.com/story/show/156412250

--- src/acceptance_tests/acceptance_tests_suite_test.go
+++ src/acceptance_tests/acceptance_tests_suite_test.go
@@ -5,9 +5,130 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+	"os"
+	"time"
+	"encoding/json"
+	"strings"
+	"os/exec"
+	"io/ioutil"
+
+	boshdir "github.com/cloudfoundry/bosh-cli/director"
+	boshlog "github.com/cloudfoundry/bosh-utils/logger"
+	"github.com/onsi/gomega/gstruct"
 )
 
 func TestAcceptanceTests(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "AcceptanceTests Suite")
+}
+
+var (
+	boshBinaryPath = "bosh"
+
+	directorURL    string
+	directorCACert string
+
+	directorClient       string
+	directorClientSecret string
+)
+
+var _ = BeforeSuite(func() {
+	setBoshEnvironmentVariables()
+
+	ensureUAAHasBeenDeployedAndRunning()
+})
+
+func setBoshEnvironmentVariables() {
+	var envFound bool
+
+	directorURL, envFound = os.LookupEnv("BOSH_ENVIRONMENT")
+	Expect(envFound).To(BeTrue(), "BOSH_ENVIRONMENT was not set")
+
+	_, envFound = os.LookupEnv("BOSH_DEPLOYMENT")
+	Expect(envFound).To(BeTrue(), "BOSH_DEPLOYMENT was not set")
+
+	directorClient, envFound = os.LookupEnv("BOSH_CLIENT")
+	Expect(envFound).To(BeTrue(), "BOSH_CLIENT was not set")
+
+	directorClientSecret, envFound = os.LookupEnv("BOSH_CLIENT_SECRET")
+	Expect(envFound).To(BeTrue(), "BOSH_CLIENT_SECRET was not set")
+
+	boshCACert, envFound := os.LookupEnv("BOSH_CA_CERT")
+	Expect(envFound).To(BeTrue(), "BOSH_CA_CERT was not set")
+
+	boshCACertContents, err := ioutil.ReadFile(boshCACert)
+	Expect(err).NotTo(HaveOccurred())
+	directorCACert = string(boshCACertContents)
+
+	director, err := buildDirector()
+	Expect(err).NotTo(HaveOccurred())
+
+	info, err := director.Info()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(info.Name).To(Equal("docker"))
+	Expect(info.User).To(Equal("admin"))
+}
+
+func ensureUAAHasBeenDeployedAndRunning() {
+	instanceInfos := getInstanceInfos(boshBinaryPath)
+	Expect(instanceInfos).ToNot(BeEmpty())
+	Expect(instanceInfos).To(ContainElement(gstruct.MatchFields(gstruct.IgnoreExtras, gstruct.Fields{
+		"InstanceGroup": Equal("uaa"),
+		"ProcessState":  Equal("running"),
+	})))
+}
+
+type instanceInfo struct {
+	IP            string
+	InstanceID    string
+	InstanceGroup string
+	Index         string
+	ProcessState  string
+}
+
+func buildDirector() (boshdir.Director, error) {
+	logger := boshlog.NewLogger(boshlog.LevelError)
+	factory := boshdir.NewFactory(logger)
+
+	config, err := boshdir.NewConfigFromURL(directorURL)
+	if err != nil {
+		return nil, err
+	}
+
+	config.Client = directorClient
+	config.ClientSecret = directorClientSecret
+	config.CACert = directorCACert
+
+	return factory.New(config, boshdir.NewNoopTaskReporter(), boshdir.NewNoopFileReporter())
+}
+
+func getInstanceInfos(boshBinary string) []instanceInfo {
+	cmd := exec.Command(boshBinary, "instances", "--details", "--json")
+	session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session, 20*time.Second).Should(gexec.Exit(0))
+
+	var response struct {
+		Tables []struct {
+			Rows []map[string]string
+		}
+	}
+
+	var out []instanceInfo
+	json.Unmarshal(session.Out.Contents(), &response)
+
+	for _, row := range response.Tables[0].Rows {
+		instanceStrings := strings.Split(row["instance"], "/")
+
+		out = append(out, instanceInfo{
+			IP:            row["ips"],
+			InstanceGroup: instanceStrings[0],
+			InstanceID:    instanceStrings[1],
+			Index:         row["index"],
+			ProcessState:  row["process_state"],
+		})
+	}
+
+	return out
 }
--- src/acceptance_tests/uaa-release_test.go
+++ src/acceptance_tests/uaa-release_test.go
@@ -2,12 +2,61 @@ package acceptance_tests_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"os/exec"
+	"github.com/onsi/gomega/gexec"
+	"time"
+	"fmt"
+	"encoding/json"
+	"strconv"
+	"strings"
+	"unicode"
 )
+
+type row struct {
+	Stdout   string `json:"stdout"`
+	ExitCode string `json:"exit_code"`
+}
+
+type table struct {
+	Rows []row
+}
+
+type sshResult struct {
+	Tables []table
+}
 
 var _ = Describe("UaaRelease", func() {
 
-	It("helo", func() {
+	It("should have loaded every certificate into the jdk truststore", func() {
+		cmd := exec.Command(boshBinaryPath, []string{"ssh", "uaa", "-c", "sudo /var/vcap/packages/uaa/jdk/bin/keytool --keystore /var/vcap/data/uaa/cert-cache/cacerts --storepass changeit -list"}...)
+		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
 
-		Fail("hello")
+		Eventually(session, 10*time.Second).Should(gexec.Exit(0))
+		output := string(session.Out.Contents())
+		Expect(output).To(MatchRegexp(fmt.Sprintf("Your keystore contains %d entries", getNumOfOSCertificates())))
 	})
 })
+
+func getNumOfOSCertificates() int {
+	caCertificatesSSHStdoutCmd := exec.Command(boshBinaryPath, []string{"--json", "ssh", "--results", "uaa", "-c", "sudo grep 'END CERTIFICATE' /etc/ssl/certs/ca-certificates.crt | wc -l"}...)
+	session, err := gexec.Start(caCertificatesSSHStdoutCmd, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session, 10*time.Second).Should(gexec.Exit(0))
+
+	var result = &sshResult{}
+	err = json.Unmarshal(session.Out.Contents(), result)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(result.Tables).To(HaveLen(1))
+	Expect(result.Tables[0].Rows).To(HaveLen(1))
+
+	numOfCerts, err := strconv.Atoi(
+		strings.TrimFunc(string(result.Tables[0].Rows[0].Stdout), func(r rune) bool {
+			return !unicode.IsNumber(r)
+		}),
+	)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(numOfCerts).To(BeNumerically(">=", 148))
+	return numOfCerts
+}

d00ad5b9ded688c4c3a572900b110f3d9a57204d
Andrew Edstrom <aedstrom@pivotal.io>
2019-02-22 11:59:24 -0800

    Fixed misconfiguration for acceptance tests CI task script
    - `uaa-deployment` expects BOSH DNS
    
    [#162794188] https://www.pivotaltracker.com/story/show/162794188
    
    Signed-off-by: Jaskanwal Pawar <jpawar@pivotal.io>
    Co-authored-by: Jaskanwal Pawar <jpawar@pivotal.io>

--- ci/tasks/test-acceptance.sh
+++ ci/tasks/test-acceptance.sh
@@ -18,7 +18,8 @@ export BOSH_BINARY_PATH="$(which bosh)"
 export BOSH_DEPLOYMENT="uaa"
 
 bosh upload-stemcell https://s3.amazonaws.com/bosh-core-stemcells/warden/bosh-stemcell-170.9-warden-boshlite-ubuntu-xenial-go_agent.tgz
-
+# BOSH DNS expected
+bosh update-runtime-config /usr/local/bosh-deployment/runtime-configs/dns.yml
 
 pushd "$ROOT_DIR/uaa-release"
     bosh create-release

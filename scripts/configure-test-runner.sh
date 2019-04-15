6b5d47c41c7b4d8d5eaac80dfc1739f8bfe306de
Ryan Richard <rrichard@pivotal.io>
2019-04-15 09:01:45 -0700

    More improvements to run-locally.sh
    - Reuse the BOSH Director (and related deployments) if it is still running
    - Don't upload the stemcell again if it is already uploaded

--- scripts/configure-test-runner.sh
+++ scripts/configure-test-runner.sh
@@ -5,7 +5,13 @@ set -eux
 source /tmp/local-bosh/director/env
 export BOSH_ENVIRONMENT="https://10.245.0.3:25555"
 
-bosh upload-stemcell https://s3.amazonaws.com/bosh-core-stemcells/warden/bosh-stemcell-170.9-warden-boshlite-ubuntu-xenial-go_agent.tgz
+stemcell_type=warden-boshlite-ubuntu-xenial-go_agent
+stemcell_version=170.9
+if [[ ! $(bosh stemcells --column=name --column=version |  grep $stemcell_type | fgrep $stemcell_version) ]]; then
+    bosh upload-stemcell https://s3.amazonaws.com/bosh-core-stemcells/warden/bosh-stemcell-${stemcell_version}-${stemcell_type}.tgz
+else
+    echo "stemcell $stemcell_version $stemcell_type already uploaded: skipping upload"
+fi
 
 cp /root/uaa-release/src/acceptance_tests/uaa-docker-deployment.yml /tmp/uaa-deployment.yml
 cp /root/uaa-release/scripts/refresh-uaa-deployment.sh /usr/local/bin/refresh
@@ -19,10 +25,8 @@ export PATH="${GOPATH}/bin:$PATH"
 go get github.com/onsi/ginkgo/ginkgo
 go install github.com/onsi/ginkgo/ginkgo
 
-
 bosh -n deploy -d uaa /tmp/uaa-deployment.yml -o "/root/uaa-release/src/acceptance_tests/opsfiles/enable-local-uaa.yml" \
 --vars-store=/tmp/uaa-store.json -v system_domain=`hostname --fqdn`
-
 
 export BOSH_GW_PRIVATE_KEY="/tmp/jumpbox_ssh_key.pem"
 export BOSH_GW_USER="jumpbox"
@@ -30,8 +34,13 @@ export BOSH_DIRECTOR_IP="10.245.0.3"
 export BOSH_BINARY_PATH="$(which bosh)"
 export BOSH_DEPLOYMENT="uaa"
 
-
 cd /root/uaa-release
-echo "feel free to run ginkgo -r ."
-echo "run refresh after test runs to restore uaa to a good deployment"
+
+set +x
+
+echo
+echo "Run 'ginkgo -v -keepGoing -r .' to run acceptance tests"
+echo "Run 'refresh' to redeploy uaa to restore to a good deployment"
+echo "Run 'exit' when you are finished"
+
 bash

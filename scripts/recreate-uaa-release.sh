3aef13e3f4d616b256084f27ad9df74a59b269c8
Joshua Casey <jcasey@pivotal.io>
2020-01-24 16:29:11 -0800

    Fix dev script
    Signed-off-by: Andrew Edstrom <aedstrom@pivotal.io>

--- scripts/recreate-uaa-release.sh
+++ scripts/recreate-uaa-release.sh
@@ -4,6 +4,6 @@ set -xeu
 
 pushd /root/uaa-release
     mkdir -p tmp
-    bosh create-release --force --tarball=tmp/uaa-dev-release.tgz
+    UAA_VERSION=0.0.0 bosh create-release --force --tarball=tmp/uaa-dev-release.tgz
     bosh upload-release tmp/uaa-dev-release.tgz
 popd
